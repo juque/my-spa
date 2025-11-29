8bae14a489e9e5a230920a6fd70d62537d9458dd
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2025-11-29 13:25:50 -0300

    add global components

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <p>Footer</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Page.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<h1>My super SPA</h1>
+	<slot />
+</template>
